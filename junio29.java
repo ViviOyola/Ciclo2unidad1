d81b15ea1b29eb633854affa93dfeac7a2b69313
ViviOyola <vima629@gmail.com>
2021-07-26 21:25:31 -0500

    practica git

---
+++ junio29.java
@@ -0,0 +1,10 @@
+plublic class junio29 {
+    public static void main(String[] args) {
+        scanner scanner = new scanner (System.in);
+        System.out.println("Dame tu nombre");
+        String nombre = scanner.nextline();
+        System.out.println("Hola" + nombre);
+    } 
+
+
+}
---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int lista [] = {1,2,8,10};
+        lista[2]=2;
+        System.out.println(lista[2]);
+    }
+}
